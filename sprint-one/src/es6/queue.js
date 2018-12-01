e00ef9b8320e86253bc893247c2c0df81cfc4653
Mike Lopez <mikeglopez.91@gmail.com>
2018-12-01 02:57:41 -0600

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+    this.front = 0;
+    this.rear = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.rear] = value;
+    this.rear++;
+    this.count++;
+  }
+
+  dequeue() {
+    var temp = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    this.count--;
+    return temp;
+  }
+
+  size() {
+    return this.count >= 0 ? this.count : 0;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  push(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  }
+
+  pop() {
+    var temp = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count--;
+    return temp;
+  }
+
+  size() {
+    return this.count >= 0 ? this.count : 0;
+  }
 }
