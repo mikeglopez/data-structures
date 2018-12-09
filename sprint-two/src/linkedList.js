0665f5ae49624105d95cb2768deaa3524a48341f
Mike Lopez <mikeglopez.91@gmail.com>
2018-12-08 23:47:07 -0600

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,39 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // TIME COMPLEXITY: O(1)
+    var node = new Node(value);
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+
+    this.tail = node;
   };
 
   list.removeHead = function() {
+    // TIME COMPLEXITY: O(1)
+    var removed = this.head;
+
+    this.head = this.head.next;
+
+    return removed.value;
   };
 
   list.contains = function(target) {
+    // TIME COMPLEXITY: O(n)
+    var curr = this.head;
+
+    while (curr) {
+      if (curr.value === target) {
+        return true;
+      }
+      curr = curr.next;
+    }
+
+    return false;
   };
 
   return list;
@@ -26,4 +53,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+    .addToTail(): O(1)
+    .removeHead(): O(1)
+    .contains(): O(n)
  */
