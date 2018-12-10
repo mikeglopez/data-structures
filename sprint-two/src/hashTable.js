de4a68626345a4a394f4ebc3541e63b0ae0a18b6
Mike Lopez <mikeglopez.91@gmail.com>
2018-12-10 04:33:04 -0600

    Complete Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,21 +6,52 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  // TIME COMPLEXITY: O(1)
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  var tuple = [k, v];
+
+  if (bucket === undefined) {
+    this._storage.set(index, [tuple]);
+  } else {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v;
+      }
+    }
+    bucket.push(tuple);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
+  // TIME COMPLEXITY: O(1)
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+
+  if (bucket !== undefined) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        return bucket[i][1];
+      }
+    }
+  } else {
+    return undefined;
+  }
 };
 
 HashTable.prototype.remove = function(k) {
+  // TIME COMPLEXITY: O(1)
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+    .insert(): O(1)
+    .retrieve(): O(1)
+    .remove(): O(1)
  */
 
 
