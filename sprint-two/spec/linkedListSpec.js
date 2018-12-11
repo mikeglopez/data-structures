05627b4f71907d74e4a25ab45ae33a0a9e8162e8
Mike Lopez <mikeglopez.91@gmail.com>
2018-12-10 18:58:34 -0600

    Complete Bare Minimum Requirements

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -23,6 +23,7 @@ describe('linkedList', function() {
     expect(linkedList.tail.value).to.equal(5);
   });
 
+  // Additional test
   it('should not designate a new head when new nodes are added', function() {
     linkedList.addToTail(4);
     expect(linkedList.head.value).to.equal(4);
